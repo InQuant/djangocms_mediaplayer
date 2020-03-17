556becc2643ae81d8e948d2c12a12126f32c5562
domlysi <dominik.lysiak@inquant.de>
2020-03-17 15:45:23 +0100

    fix template video

---
+++ djangocms_mediaplayer/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'djangocms_mediaplayer.apps.DjangocmsMediaplayerConfig'
--- djangocms_mediaplayer/apps.py
+++ djangocms_mediaplayer/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class DjangocmsMediaplayerConfig(AppConfig):
     name = 'djangocms_mediaplayer'
+    verbose_name = _('DjangoCMS Media Player')
