4c387e0cb01cf6dfd1671d8ce3fcc7f4576d0981
domlysi <dominik.lysiak@inquant.de>
2020-04-29 08:43:15 +0200

    module name added

--- mediaplayer/cms_plugins.py
+++ mediaplayer/cms_plugins.py
@@ -10,6 +10,7 @@ class VideoPlayerPlugin(PlusPluginBase):
     name = _('Video Player')
     form = VideoPlayerForm
     render_template = 'djangocms_mediaplayer/videoplayer_plugin.html'
+    module = _('Media Player')
 
 
 @plugin_pool.register_plugin
@@ -17,3 +18,4 @@ class AudioPlayerPlugin(PlusPluginBase):
     name = _('Audio Player')
     form = AudioPlayerForm
     render_template = 'djangocms_mediaplayer/audioplayer_plugin.html'
+    module = _('Media Player')
